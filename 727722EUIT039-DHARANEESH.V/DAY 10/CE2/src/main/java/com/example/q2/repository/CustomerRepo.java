80ce156b90d65579fc1b2c553597f728ca8e141b
Dharaneesh.V <124854722+dharaneesh-v@users.noreply.github.com>
2024-03-08 12:09:11 +0530

    CustomerRepo.java

--- 727722EUIT039-DHARANEESH.V/DAY 10/CE2/src/main/java/com/example/q2/repository/CustomerRepo.java
+++ 727722EUIT039-DHARANEESH.V/DAY 10/CE2/src/main/java/com/example/q2/repository/CustomerRepo.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.q2.model.Customer;
 
 public interface CustomerRepo extends JpaRepository<Customer,Integer>{
-
+    @Query("select c from Customer c where c.city=?1")
     List<Customer> findByCity(String city);
 } 
